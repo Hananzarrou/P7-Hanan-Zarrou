cc382f6ef8794aa74a3c8be2472fa83272b2a5fa
hanan <hananzarrou@gmail.com>
2021-05-23 18:07:32 +0200

    add vuex

---
+++ frontend/src/store/index.js
@@ -0,0 +1,17 @@
+import { createStore } from 'vuex'
+
+
+
+export default createStore({
+  state: {
+        month: 7,
+        day: 23,
+        year: 2010
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
