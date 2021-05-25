7f99d1880760aa54679f5de77fc8ba2e31a221a4
hanan <hananzarrou@gmail.com>
2021-05-25 13:59:35 +0200

    add footer

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="footer bg-light font-weight-bold text-secondary">
+        <footer>
+            
+            <ul class="list-inline">
+                <li class="list-inline-item"><a class="font-weight-bold text-secondary" href="#">Mentions légales</a></li>
+                <li class="list-inline-item"><a class="font-weight-bold text-secondary" href="#">Signaler un bug</a></li>
+                <li class="list-inline-item"><a class="font-weight-bold text-secondary" href="/myprofile">voir mon  profile</a></li>
+            </ul>
+            <p class="copyright">Groupomania © 2021</p>
+        </footer>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Footer',
+}
+</script>
+
+<style scoped>
+ .footer {
+    margin-top: 20px;
+ }
+</style>
---
+++ frontend/src/components/about.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="about"> 
+    <h1>Bienvenue chez Groupomania </h1>
+    <h2> Echanger entre vous ! c'est le facteur clé du reuissite ...</h2>
+</div>  
+</template>
+
+<script>
+export default {
+    name: 'About',
+}
+</script>
+
+<style scoped>
+    h1, h2 {
+        color: #000000;
+        font-weight: bold;
+    }
+</style>
