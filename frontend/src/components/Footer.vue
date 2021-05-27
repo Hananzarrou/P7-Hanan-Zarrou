50cace3952717735c272598c909e5b6ba52605f3
hanan <hananzarrou@gmail.com>
2021-05-27 21:15:36 +0200

    import footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -20,7 +20,7 @@ export default {
 </script>
 
 <style scoped>
- .footer {
+ .Footer {
     margin-top: 20px;
  }
 </style>
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
     <Seconnecter />
+     <Footer />
   </div>
 </template>
 <script>
 
 import Seconnecter from "../components/Seconnecter.vue";
+import Footer from '../components/Footer.vue'
+
 export default {
   name: "login",
   components: {
     Seconnecter,
+    Footer
   },
+
 };
 </script>
 <style lang="scss"></style>
