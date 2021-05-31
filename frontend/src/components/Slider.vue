9a285cbb82a59dee79213e156157d402086f1c1a
hanan <hananzarrou@gmail.com>
2021-05-31 12:08:46 +0200

    modifpost

---
+++ frontend/src/components/Slider.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="3000"
+      controls
+      indicators
+      background="#ccc"
+      style="text-shadow: 1px 1px 2px #000;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+    <b-carousel-slide>
+      <template  v-slot:img>
+        <img
+            class="d-block img-fluid w-100"
+            width="1024"
+            height="480"
+            src="@/assets/build-groupomania.jpg"
+            alt="image slot"
+          >
+          </template>
+      </b-carousel-slide>
+      <b-carousel-slide>
+      <template  v-slot:img>
+        <img
+            class="d-block img-fluid w-100"
+            width="1024"
+            height="480"
+            src="@/assets/slide-groupomania.png"
+            alt="image slot"
+          >
+          </template>
+      </b-carousel-slide>
+      <b-carousel-slide>
+      <template  v-slot:img>
+        <img
+            class="d-block img-fluid w-100"
+            width="1024"
+            height="480"
+            src="@/assets/slide-main.png"
+            alt="image slot"
+          >
+          </template>
+      </b-carousel-slide>
+    </b-carousel>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        slide: 0,
+        sliding: null
+      }
+    },
+    methods: {
+      onSlideStart() {
+        this.sliding = true
+      },
+      onSlideEnd() {
+        this.sliding = false
+      }
+    }
+  }
+</script>
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,4 +1,24 @@
 <template>
+  <div class="login">
+   
+  <Navbar />
+  <Log />
+  <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+import Log from '@/components/Log.vue'
+import Footer from '@/components/Footer.vue'
+export default {
+  name: 'Login', 
+  components: {
+  Navbar, Log, Footer    
+}
+}
+</script>
+<!--<template>
   <div id="app">
     <Seconnecter />
      <Footer />
@@ -18,4 +38,4 @@ export default {
 
 };
 </script>
-<style lang="scss"></style>
+<style lang="scss"></style>-->
