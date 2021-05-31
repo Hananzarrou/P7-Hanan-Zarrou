966af28b4f84d72fdf7d023130e145dd15ca2020
hanan <hananzarrou@gmail.com>
2021-05-31 11:33:44 +0200

    add post#frt

---
+++ frontend/src/components/Post.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="row mx-auto my-5">
+        <div class="col-lg-8 mx-auto bg-primary ">
+            <div>
+                <img class="rounded-circle" width="50" src="https://picsum.photos/80/80/?random?image=4">
+            </div>
+            <div class="text-white font-weight-bold">
+                {{ pseudo + " " + "a écrit"}}
+            </div>
+			<div class="card my-3 mx-auto">
+                {{ content }} 
+                <div>
+            <img id="imgpost" :src="imageUrl">
+            </div>
+            </div>
+        <slot name="Comments"></slot>
+        <slot name="EditCom"></slot>
+         
+  </div>      
+</div>
+</template>
+
+<script>
+export default {
+name: "Post",
+data() {
+    return {
+    }
+},
+    props: {
+        
+        pseudo: {
+            type: String,
+            default: ""
+        },
+        content: {
+            type: String,
+            default: ""
+        },
+        postId: {
+            type: String
+        },
+        imageUrl: {
+            type: String
+        },
+        commentContent: {
+            type: String
+        }
+    },
+}
+</script>
+
+<style scoped>
+#imgpost {
+ max-width: 200px;
+ max-height: 200px;
+}
+</style>
