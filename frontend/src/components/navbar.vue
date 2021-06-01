0b94b1f63284f51f70f125bd8c588b8dc3b5d765
hanan <hananzarrou@gmail.com>
2021-06-01 21:18:17 +0200

    problems frt

--- frontend/src/components/navbar.vue
+++ frontend/src/components/navbar.vue
@@ -1,123 +1,42 @@
 <template>
-<div class="login col-sm-6 mx-auto bg-white my-50">
-    <h3 class="text-secondary">Me connecter</h3>
+    <div class="Navbar">
+<b-navbar toggleable="lg" type="dark" variant="primary">
+    <a href="/"><img class="img-fluid logo" src="../assets/icon-left-font-monochrome-black.svg" aria-label="logo groupomania" alt="Logo groupomania"></a>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-<form id="form"
-    @submit.prevent="login()"
-    method="post"
-    novalidate="true">
-<div class="form-group form-group-sm" :class="{ 'form-group--error': $v.email.$error }">
-    <div class="col-sm-4 mx-auto">
-        <label for="email">Email</label>
-    <div class="input-group">
-        <span class="input-group-addon bg-info">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" class="bi bi-person-fill" viewBox="2 1 10 13">
-            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-        </svg></span>
-        <input
-            id="email"
-            name="email"
-            type="email"
-            class="form-control form-control-sm"
-            v-model.trim="$v.email.$model"
-            >
-        </div>
-        <span class="error" v-if="!$v.email.required">Champ email manquant</span>
-    </div>
-    </div>
-<div class="form-group form-group-sm" :class="{ 'form-group--error': $v.password.$error }">
-    <div class="col-sm-4 mx-auto">
-        <label for="password">Mot de passe</label>
-    <div class="input-group">
-        <span class="input-group-addon bg-info">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-lock2-fill" viewBox="0 0 16 16">
-            <path d="M7 6a1 1 0 0 1 2 0v1H7V6z"/>
-            <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm-2 6v1.076c.54.166 1 .597 1 1.224v2.4c0 .816-.781 1.3-1.5 1.3h-3c-.719 0-1.5-.484-1.5-1.3V8.3c0-.627.46-1.058 1-1.224V6a2 2 0 1 1 4 0z"/>
-            </svg>
-        </span>
-        <input
-            id="password"
-            name="password"
-            type="password"
-            class="form-control form-control-sm"
-            v-model.trim="$v.password.$model">
-        </div>
-        <span class="error" v-if="!$v.password.required">Champ mot de passe manquant</span> <br>
-        <span class="error" v-if="!$v.password.minLength">{{$v.password.$params.minLength.min}} caractères min !.</span>
-    </div>
-    </div>
-    
-    <button class="btn btn-dark btn-sm" type="submit" @click.prevent="login">Connexion</button>
-    </form>
-    <p> Pas encore inscrit ? Créez votre compte dès maintenant !</p>
-    <button class="btn btn-dark btn-sm" @click.prevent="goSignin" >Inscription</button>
+    <b-collapse id="nav-collapse" is-nav>
+    <b-navbar-nav class="ml-auto">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" class="bi bi-person-fill" viewBox="2 1 10 13">
+<path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+</svg>
+        <b-nav-item size="sm" class="mr-sm-2 font-weight-bold"><router-link to="/login" class="nav-link">Se connecter</router-link></b-nav-item>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+<path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+</svg>
+        <b-nav-item size="sm" class="mr-sm-2 font-weight-bold"><router-link to="/signup" class="nav-link">S'inscrire</router-link></b-nav-item>
+    </b-navbar-nav>
+
+    </b-collapse>
+</b-navbar>
 </div>
 </template>
 
-
 <script>
-import {required,minLength} from "vuelidate/lib/validators"; 
-import axios from "axios";
 export default {
-name: 'login',
-data() {
-    return {
-    email: "",
-    password: "",
-    submited: false,
-        }
-    },
-validations: {
-    email: {
-        required,
-    },
-    password: {
-        required,
-        minLength: minLength(6)
-    }
-},
-methods:{
-  
-    login() {
-    axios.post( 'http://localhost:3000/api/auth/login', {
-        email: this.email,
-        password: this.password,
-        })
-    
-        .then((res) => {
-                localStorage.setItem("token",   res.data.token)
-                localStorage.setItem("userId",  res.data.userId)
-                console.log(res);
-                alert("Bienvenue ! Vous êtes connecté ! ");
-                this.$router.push('/myprofile');           
-            })
-            
-        .catch(error => {
-        console.log("Identifiants invalides !" + (error));
-        })
-    },
-    goSignin(){
-        this.$router.push('Signup');
-    }
+    name: 'Navbar',
+    user: false
 }
-} 
 </script>
 
 <style scoped>
-.signin {
-    margin-top: 20px;
+.navbar-dark .navbar-nav .nav-link {
+    color: white;
 }
-.btn {
-    margin-bottom: 20px;
-    font-weight: bold;
-}
-.error {
-    color: red;
-    font-weight: bold;
+.logo {
+    max-height: 100px;
+    max-width: 200px;
 }
 </style>
-
-
 
 <!--<template>
   component de navigation dans l application-
