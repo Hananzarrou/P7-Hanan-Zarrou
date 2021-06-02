384506b1ed942747c1405ed5c930e6d58f345197
hanan <hananzarrou@gmail.com>
2021-06-02 13:07:55 +0200

    constsequelize

---
+++ backend/db.config.js
@@ -0,0 +1,30 @@
+const mysql = require('mysql');
+const { Sequelize }  = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize({
+
+    host: 'DB_HOST',
+ 
+    user: 'DB_USERNAME',
+ 
+    password: 'DB_PASS',
+
+    database: 'bas_groupomania',
+
+    dialect: "mysql"
+
+  });
+ 
+    
+
+  const db = {};
+try {
+  db;
+  console.log('Connecté à la base de données MySQL!');
+} catch (error) {
+  console.error('Impossible de se connecter, erreur suivante :', error);
+}
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
