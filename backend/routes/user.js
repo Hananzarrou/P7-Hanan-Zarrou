b528de6bf6fee31ca0f39a2ae6a8b2b8aa8c6a37
hanan <hananzarrou@gmail.com>
2021-06-02 12:43:08 +0200

    riutes#2

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,30 @@
+// Import
 const express = require('express');
+const router = express.Router();
+const userCtrl = require('../controllers/user');
+const passValidate = require('../middleware/passValidate');
+const mailValidate = require('../middleware/mailValidate');
+const rateLimit = require("express-rate-limit");
+const auth = require('../middleware/auth');
+
+const rateLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000, 
+  max: 5, // 
+  message: " Trop de tentatives échouées, réessayez dans 5 minutes",
+});
+
+
+// Routes
+
+router.post('/signup', mailValidate, passValidate, userCtrl.signup);
+router.post('/login', rateLimiter, userCtrl.login);
+router.delete('/:id', userCtrl.delete);
+router.put('/:id', auth, userCtrl.update);
+
+module.exports = router;
+
+
+/*const express = require('express');
 const router = express.Router();
 
 const userCtrl = require('../controllers/user');
@@ -6,4 +32,4 @@ const userCtrl = require('../controllers/user');
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 
-module.exports = router;
+module.exports = router;*/
