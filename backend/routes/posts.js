fbd6c9ca2debcfbe64403dd2eea304bf892a505e
hanan <hananzarrou@gmail.com>
2021-06-02 12:49:45 +0200

    route#9

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -4,7 +4,21 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
-const sauceCtrl = require('../controllers/sauce');
+const postsCtrl = require('../controllers/post');
+
+router.post('/', auth, multer, postsCtrl.createPost);
+router.get('/', auth, postsCtrl.findAll);
+router.put('/:id', auth, multer, postsCtrl.modifyPost);
+router.delete('/:id', auth, postsCtrl.deletePost);
+
+module.exports = router;
+/*const express = require('express');
+const router = express.Router();
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+const sauceCtrl = require('../controllers/post');
 
 router.get('/', auth, sauceCtrl.getAllSauce);
 router.post('/', auth, multer, sauceCtrl.createSauce);
@@ -13,4 +27,4 @@ router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 router.post('/:id/like', auth, sauceCtrl.addLike);
 
-module.exports = router;
+module.exports = router;*/
